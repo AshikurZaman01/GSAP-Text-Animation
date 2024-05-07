fd5610706e5b0f85a394893e8a167e1e0ad6be0a
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-05-07 22:37:07 +0600

    updated

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,8 +1,33 @@
+import { useEffect } from "react";
 
 const Home = () => {
+
+
+    useEffect(() => {
+
+        const h1 = document.querySelector('h1');
+        const h1Text = h1.innerText;
+
+        const splittedText = h1Text.split('');
+
+        let empty = "";
+
+        splittedText.forEach((letter) => {
+            empty += letter;
+            empty += letter;
+
+        })
+
+        h1.innerText = empty;
+
+    }, [])
+
+
     return (
-        <div>
-            this is home
+        <div className="flex justify-center items-center h-screen bg-slate-900">
+            <div>
+                <h1 className="text-9xl font-semibold text-gray-500">Bangladesh</h1>
+            </div>
         </div>
     );
 };
